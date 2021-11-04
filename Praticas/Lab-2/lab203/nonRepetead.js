61b1d57bd116675a09dc16081c368867a0aa1fe9
daniff15 <dani.fig@ua.pt>
2021-11-04 10:09:58 +0000

    lab203 comp

--- Praticas/Lab-2/lab203/nonRepetead.js
+++ Praticas/Lab-2/lab203/nonRepetead.js
@@ -2,13 +2,13 @@ nonRepetead = function (){
     var fullNumber = db.phones.find({},{"display": 1, "_id": 0}).toArray();
 
     var nonRepeating = []
-    for (let index = 0; index < fullNumber.length; index++) {
-        const element = fullNumber[index].display;
+    for (var index = 0; index < fullNumber.length; index++) {
+        var element = fullNumber[index].display;
 
         element = element.split("-")[1]
         var tempNumber = []
         var nonRepetead = true;
-        for (let idx = 0; idx < element.length; idx++) {
+        for (var idx = 0; idx < element.length; idx++) {
             if (tempNumber.includes(element[idx])) {
                 nonRepetead = false
                 break
@@ -18,7 +18,7 @@ nonRepetead = function (){
         }
 
         if (nonRepetead){
-            nonRepeating.push(fullNumber[i])
+            nonRepeating.push(fullNumber[index])
         }
 
     }
