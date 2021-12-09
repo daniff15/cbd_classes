f11698a372f1a8c827a1de348b34f3333b652508
daniff15 <dani.fig@ua.pt>
2021-12-09 11:05:04 +0000

    init e3

--- Praticas/Lab-3/Lab-3/3/my-app/src/main/java/com/mycompany/app/App.java
+++ Praticas/Lab-3/Lab-3/3/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,39 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
 public class App 
 {
+    private static Session session;
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            Cluster cluster = Cluster.builder().addContactPoints("localhost").build();
+
+            session = cluster.connect("cbd_lab3_2");
+
+            //Insert a user
+            //insertUser("novoUser", "Novo User", "novo@gmail.com", "2021-12-09 10:38:28.030000+0000");
+
+        }
+        catch(Exception e){
+            System.err.println("Error connecting to Cassandra database: " + e);
+        }
+    }
+
+
+    public static void insertUser(String username, String name, String email, String timestamp){
+        try {
+            StringBuilder sb = new StringBuilder("INSERT INTO ")
+                    .append("user").append("(email, name, registration_date, username) ")
+                    .append("VALUES (").append(email).append(", '" + name + "'").append(", '" + timestamp + "'").append(", '" + username + "'");
+            String query = sb.toString();
+            session.execute(query);
+            System.out.println("User insertado com sucesso!");
+        }catch (Exception e){
+            System.err.println("Error inserting into Cassandra database: " + e);
+        }
     }
 }
